81e6b539ff23823ff6a2399d52b74c5dbc6f01aa
nikhilsingh38 <93506721+nikhilsingh38@users.noreply.github.com>
2022-04-16 23:22:29 +0530

    hanum

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,22 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore,combineReducers, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import reduxThunk from "redux-thunk";
 import rootReducer from "./root-reducer";
+import { cityReducer } from "./City/reducer";
+import { countryReducer } from "./Country/reducer";
+
 
 const middlewares = [reduxThunk];
 
 if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
+
+  // const rootReducer = combineReducers({
+  //   city: cityReducer,
+  //   country: countryReducer,
+  // });
+//export const store = createStore(rootReducer, enhancer);
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
