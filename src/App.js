e2951703117f5ae9239cc9d4697948d89e221abf
nikhilsingh38 <93506721+nikhilsingh38@users.noreply.github.com>
2022-04-16 23:05:23 +0530

    hanu

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import AddUser from "./pages/AddUser";
 import EditUser from "./pages/EditUser";
+import { AddCountry } from "./pages/AddCountry";
+import { AddCity } from "./pages/AddCity";
 
 function App() {
   return (
     <div className="App">
+      <Link to="/">Home</Link>
+      <Link to="/add-country">Add-Country</Link>
+      <Link to="/add-city"> Add-City</Link>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/addUser" component={AddUser} />
-       
+        <Route path={"/add-country"} component={AddCountry} />
+        <Route path={"/add-city"} component={AddCity} />
         <Route exact path="/editUser/:id" component={EditUser} />
       </Switch>
     </div>
